991958d1c1e90acdbd275d11a5d1a4eb9edd62c8
M. Greyson Christoforo <grey@christoforo.net>
2024-11-10 14:41:33 -0700

    add some text

--- airthings_plot/plot.py
+++ airthings_plot/plot.py
@@ -27,17 +27,22 @@ for datafilepath in here.joinpath("data").glob("*.csv"):
     raw_radon_split.loc[~(raw_radon_split["red"]>=150), "red"] = np.nan
     raw_radon_split.loc[~(raw_radon_split["green"]<100), "green"] = np.nan
     
-    ax = raw_radon_split.plot(y=["green", "yellow", "red"], color=["green", "yellow", "red"], label=["good", "bad", "ugly"], title="Radon in Office", ylabel=radon_col, xlabel="When", grid=True)
+    ax = raw_radon_split.plot(y=["green", "yellow", "red"], color=["green", "yellow", "red"], label=["good", "bad", "ugly"], title="Radon in 2808", ylabel=radon_col, xlabel="When", grid=True)
     ax.set_facecolor("silver")
     ax.grid(color='w')
+    ax.set_ylim(bottom=0)
 
 
     fantimes = []
-    fantimes.append({"event": "dinky fan on",             "local_time": "2024-10-14T16:00:00.000", "y":0.35})
-    fantimes.append({"event": "big fan on",               "local_time": "2024-10-30T21:00:00.000", "y":0.60})
-    fantimes.append({"event": "moved to basement desk",   "local_time": "2024-11-10T14:00:00.000", "y":0.60})
+    fantimes.append({"event": "sensor @ ground floor office",  "local_time": None, "y":0.65})
+    fantimes.append({"event": "pipe install and dinky fan on", "local_time": "2024-10-14T16:00:00.000", "y":0.65})
+    fantimes.append({"event": "big fan swapped in",            "local_time": "2024-10-30T21:00:00.000", "y":0.9})
+    fantimes.append({"event": "sensor @ basement desk",        "local_time": "2024-11-10T14:00:00.000", "y":0.65})
     for fantime in fantimes:
-        fanline_x = pd.to_datetime(fantime["local_time"], utc=False).tz_localize(tz_string)
+        if fantime["local_time"]:
+            fanline_x = pd.to_datetime(fantime["local_time"], utc=False).tz_localize(tz_string)
+        else:
+            fanline_x = raw.index[0]
         fanline = ax.vlines(fanline_x, ymin=ax.get_ylim()[0], ymax=ax.get_ylim()[1], label="dinky fan", color="black", linestyles='dashed')
         ax.text(fanline_x, fantime["y"], fantime["event"], color='k', ha='right', va='top', rotation=90, transform=ax.get_xaxis_transform())
     #ax.legend()
